5c29d523e0536cbb060c2fa7027129217da674b8
BryantJimenez <bryant.luis1600186@gmail.com>
2020-05-04 08:40:51 -0400

    Migraciones y seeders version 2

--- database/seeds/SizesTableSeeder.php
+++ database/seeds/SizesTableSeeder.php
@@ -12,14 +12,14 @@ class SizesTableSeeder extends Seeder
     public function run()
     {
         $sizes = [
-    		['id' => 1, 'name' => 'Pequeña', 'slug' => 'pequena', 'price' => 48.0],
-    		['id' => 2, 'name' => 'Madiana', 'slug' => 'mediana', 'price' => 65.0],
-    		['id' => 3, 'name' => 'Grande', 'slug' => 'grande', 'price' => 75.0],
-    		['id' => 4, 'name' => 'Familiar', 'slug' => 'familiar', 'price' => 94.0],
-            ['id' => 5, 'name' => 'Super', 'slug' => 'super', 'price' => 104.0],
-            ['id' => 6, 'name' => 'Gigante', 'slug' => 'gigante', 'price' => 209.0],
-            ['id' => 7, 'name' => 'Super Gigante', 'slug' => 'super-gigante', 'price' => 245.0],
-    		['id' => 8, 'name' => 'Normal', 'slug' => 'normal', 'price' => NULL]
+    		['id' => 1, 'name' => 'Unico', 'slug' => 'unico'],
+            ['id' => 2, 'name' => 'Pequeña', 'slug' => 'pequena'],
+    		['id' => 3, 'name' => 'Madiana', 'slug' => 'mediana'],
+    		['id' => 4, 'name' => 'Grande', 'slug' => 'grande'],
+    		['id' => 5, 'name' => 'Familiar', 'slug' => 'familiar'],
+            ['id' => 6, 'name' => 'Super', 'slug' => 'super'],
+            ['id' => 7, 'name' => 'Gigante', 'slug' => 'gigante'],
+            ['id' => 8, 'name' => 'Super Gigante', 'slug' => 'super-gigante']
     	];
     	DB::table('sizes')->insert($sizes);
     }
