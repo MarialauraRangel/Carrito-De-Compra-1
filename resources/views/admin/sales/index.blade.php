1b11715117c8c1207aad52e657f52e15430a1ed8
BryantJimenez <bryant.luis1600186@gmail.com>
2020-05-05 00:03:15 -0400

    Arreglando error de rutas en js

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -70,7 +70,7 @@
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Introduzca el caero y repartidor correspondiente</h5>
+				<h5 class="modal-title">Introduzca el cajero y repartidor correspondiente</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
